77ff9262192fd51c08284b18c8c6dd4fd07e52c2
Maida1610 <maidaisrar19@gmail.com>
2026-05-02 13:32:37 +0500

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,14 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  plugins: [react()],
-  server: {
-     origin: 'http://localhost:5173',
-  },
+     
+ 
+  plugins: [
+   
+    react(),
+     tailwindcss(),
+  ],
+ 
 })
