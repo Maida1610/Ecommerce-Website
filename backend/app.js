aa05fd41e5e81f9c3071bf4826553d5c8558d8c4
Maida1610 <maidaisrar19@gmail.com>
2026-05-02 11:22:33 +0500

    updated

--- backend/app.js
+++ backend/app.js
@@ -5,50 +5,54 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-// Middleware
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(
-  cors({
-    origin: "http://localhost:5173/",
-    credentials: true,
-  })
-);
-app.use("/", express.static("uploads"));
+app.use(cors({
+  origin: ['http://localhost:5173',],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  credentials: true,
+   allowedHeaders: ["Content-Type", "Authorization"],
+}));
 
-// Config
-if (process.env.NODE_ENV !== "production") {
+app.use(cookieParser());
+app.use("/",express.static(require("path").join(__dirname, "uploads")));
+app.use("/test", (req, res) => {
+  res.send("Hello world!");
+});
+
+//app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
+// config
+if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({
-    path: "server/config/.env",
+    path: "config/.env",
   });
 }
 
-// Import routes
+// import routes
 const user = require("./controller/user");
 const shop = require("./controller/shop");
 const product = require("./controller/product");
 const event = require("./controller/event");
-const coupon = require("./controller/couponCode");
+const coupon = require("./controller/coupounCode");
 const payment = require("./controller/payment");
 const order = require("./controller/order");
-const conversations = require("./controller/conversation");
+const conversation = require("./controller/conversation");
 const message = require("./controller/message");
 const withdraw = require("./controller/withdraw");
 
-// Mount routes
 app.use("/api/v2/user", user);
+app.use("/api/v2/conversation", conversation);
+app.use("/api/v2/message", message);
+app.use("/api/v2/order", order);
 app.use("/api/v2/shop", shop);
 app.use("/api/v2/product", product);
 app.use("/api/v2/event", event);
 app.use("/api/v2/coupon", coupon);
 app.use("/api/v2/payment", payment);
-app.use("/api/v2/order", order);
-app.use("/api/v2/conversation", conversations);
-app.use("/api/v2/message", message);
 app.use("/api/v2/withdraw", withdraw);
 
-// Error handling middleware
+// it's for ErrorHandling
 app.use(ErrorHandler);
 
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,41 @@
 const app = require("./app");
-const connectDatabase = require("./db/database");
+const connectDatabase = require("./db/Database");
+const cloudinary = require("cloudinary");
 
-//Handling uncaught Exceptions
+// Handling uncaught Exception
 process.on("uncaughtException", (err) => {
   console.log(`Error: ${err.message}`);
-  console.log(`Shutting down the server due to uncaught exceptions`);
+  console.log(`shutting down the server for handling uncaught exception`);
 });
 
-//config
-if (process.env.NODE_ENV !== "production") {
+// config
+if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({
-    path: "server/config/.env",
+    path: "config/.env",
   });
 }
 
-//connect db
+// connect db
 connectDatabase();
 
-//create server
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
+
+// create server
 const server = app.listen(process.env.PORT, () => {
   console.log(
     `Server is running on http://localhost:${process.env.PORT}`
   );
 });
 
-//unhandled promise rejection
+// unhandled promise rejection
 process.on("unhandledRejection", (err) => {
-  console.log(`Shutting down the server due to unhandled promise rejection: ${err.message}`);
+  console.log(`Shutting down the server for ${err.message}`);
+  console.log(`shutting down the server for unhandle promise rejection`);
 
   server.close(() => {
     process.exit(1);
