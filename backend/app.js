3faee408e4fb48680e02fd4fbbcd7ace420f4bef
Maida1610 <maidaisrar19@gmail.com>
2026-05-16 15:39:30 +0500

    updated

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,9 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 app.use(cors({
-  origin: ['http://localhost:5173',],
+  origin: ['http://localhost:5173',
+    'https://ecommerce-website-frontend-lyart.vercel.app',
+  ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   credentials: true,
    allowedHeaders: ["Content-Type", "Authorization"],
