80063e5630787a92526f8576ae494df369af89f5
Maida Israr <maidaisrar19@gmail.com>
2026-06-28 23:13:01 -0700

    update backend path

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,2 +1,2 @@
-export const server = import.meta.env.VITE_SERVER_URL || "http://localhost:8000/api/v2";
-export const backendUrl = import.meta.env.VITE_BACKEND_URL || "http://localhost:8000/";
+export const server = import.meta.env.VITE_SERVER_URL || "https://ecommerce-website-ten-black.vercel.app/api/v2";
+export const backendUrl = import.meta.env.VITE_BACKEND_URL || "https://ecommerce-website-ten-black.vercel.app/";
